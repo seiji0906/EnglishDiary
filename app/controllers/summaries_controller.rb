fcc6bdd0e119120b5393720fa4676a61131885ab
seiji0906 <runrunrun3838@icloud.com>
2020-09-14 03:30:01 +0900

    form design

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -7,7 +7,7 @@ class SummariesController < ApplicationController
   def create
     @summary = Summary.new(summaries_params)
     if @summary.save
-      redirect_to "/"
+      redirect_to summaries_path
     end
   end
 
